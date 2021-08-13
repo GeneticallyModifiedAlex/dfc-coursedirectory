2fa57f4ae05bf60e67c0e67ea3245e8e9cb6b56f
James Gunn <james@gunn.io>
2021-08-13 12:36:57 +0100

    Fix up some delivery mode-dependent bad data in FAC index

---
+++ src/Dfc.CourseDirectory.Functions/FixFacIndexDeliveryModeFields.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Dapper;
+using Dfc.CourseDirectory.Core.DataStore.Sql;
+using Microsoft.Azure.WebJobs;
+
+namespace Dfc.CourseDirectory.Functions
+{
+    public class FixFacIndexDeliveryModeFields
+    {
+        private readonly ISqlQueryDispatcherFactory _sqlQueryDispatcherFactory;
+
+        public FixFacIndexDeliveryModeFields(ISqlQueryDispatcherFactory sqlQueryDispatcherFactory)
+        {
+            _sqlQueryDispatcherFactory = sqlQueryDispatcherFactory;
+        }
+
+        [FunctionName(nameof(FixFacIndexDeliveryModeFields))]
+        [NoAutomaticTrigger]
+        public async Task Execute(string input)
+        {
+            using var dispatcher = _sqlQueryDispatcherFactory.CreateDispatcher();
+
+            var sql = @"
+UPDATE Pttcd.FindACourseIndex SET [National] = 1 WHERE DeliveryMode = 2
+
+UPDATE Pttcd.FindACourseIndex SET StudyMode = NULL WHERE DeliveryMode <> 1";
+
+            await dispatcher.Transaction.Connection.ExecuteAsync(sql, transaction: dispatcher.Transaction);
+
+            await dispatcher.Commit();
+        }
+    }
+}
